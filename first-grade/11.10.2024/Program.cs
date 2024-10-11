2e231007dc22d66ff5e81bcb6c0bbf0f82453c52
Alexander <yourflexium@protonmail.com>
2024-10-11 13:42:49 +0600

    work: 11.10.2024

---
+++ first-grade/11.10.2024/Program.cs
@@ -0,0 +1,28 @@
+﻿int n = int.Parse(Console.ReadLine());
+bool isOver = false;
+
+int reversedN = 0,
+	nowN = 0;
+
+if (n <= 0)
+{
+	Console.WriteLine("Вы ввели отрицательное / нулевое число");
+	isOver = true;
+}
+
+while(!isOver)
+{
+	if (n == 0)
+	{
+		isOver = true;
+		break;
+	}
+
+	nowN = n % 10;
+	n = n / 10;
+	if (nowN % 2 != 0)
+	{
+		reversedN = reversedN * 10 + nowN;
+	}
+}
+Console.WriteLine(reversedN);
