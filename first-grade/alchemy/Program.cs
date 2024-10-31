2feaf92aab866e3967499711dc34e7ddb4a6aece
Alexander <yourflexium@proton.me>
2024-10-31 19:29:44 +0600

    31.10.2024: rewriting ts and adding answer on csharp

--- first-grade/alchemy/Program.cs
+++ first-grade/alchemy/Program.cs
@@ -9,124 +9,49 @@ DUST Thj ispDFgNvR nzenfPXTLUrEXc 6 stBPuNQbYTCgYRc gEjGoqQEKLdE
 FIRE vNCXvSoTf 6
 MIX 4 5 7 8 9";
 
-bool IsConvertibleToNumber(string value)
+List<string> operations = new List<string>();
+
+string Wrapper(string resString, int numberOperation)
 {
-    // Проверка на null или пустую строку
-    if (string.IsNullOrEmpty(value))
-    {
-        return false;
-    }
-
-    // Попробуем конвертировать в int
-    if (int.TryParse(value, out _))
-    {
-        return true;
-    }
-
-    // Попробуем конвертировать в double
-    if (double.TryParse(value, out _))
-    {
-        return true;
-    }
-
-    // Если не удалось преобразовать в ни один из типов
-    return false;
-}
-
-
-
-string[] operations = new string[line.Length];
-
-string Wrapper(string resString, number numberOperation) {
-	resString += operations[numberOperation - 1];
-	return resString;
+    return resString + operations[numberOperation - 1];
 }
 
 string[] lineArrayForm = line.Split('\n');
 
-for(int i = 0; i < lineArrayForm.Length; i++) {
-	string[] currentLine = lineArrayForm[i].Split(' ');
+foreach (string currentLine in lineArrayForm)
+{
+    string[] parts = currentLine.Split(' '); 
 
-	string resString = "";
+    List<string> cleanedParts = new List<string>();
 
-	switch (currentLine[0]) {
-		case "DUST":
-			Console.WriteLine($"--- dust ---");
+    foreach (var part in parts) // Элемент может быть либо типа string, либо типа number
+    {
+        if (!string.IsNullOrWhiteSpace(part))
+        {
+            cleanedParts.Add(part);
+        }
+    }
 
-			resString += "DT";
+    string type = cleanedParts[0];
+    string code = type == "DUST" ? "DT" : type == "WATER" ? "WT" : type == "FIRE" ? "FR" : "MX";
+    string endCode = type == "DUST" ? "TD" : type == "WATER" ? "TW" : type == "FIRE" ? "RF" : "XM";
+    string resString = code;
 
-			for (int j = 1; j < currentLine.Length; j++) {
-				string currentEntry = currentLine[j];
-				if (!IsConvertibleToNumber(currentLine[j])) {
-					resString += currentLine[j];
-				} else {
-					resString = Wrapper(resString, int.Parse(currentLine[j]));
-				}
-			}
-			resString += 'TD';
-			operations[operations.length] = resString;
-			resString = '';
+    for (int j = 1; j < cleanedParts.Count; j++)
+    {
+        if (int.TryParse(cleanedParts[j], out int number))
+        {
+            resString = Wrapper(resString, number);
+        }
+        else
+        {
+            resString += cleanedParts[j];
+        }
+    }
 
-			console.log(operations);
-			break;
+    resString += endCode;
+    operations.Add(resString);
 
-		case 'WATER':
-			console.log('--- water ---');
-
-			resString += 'WT';
-
-			for (let j = 1; j < currentLine.length; j++) {
-				if (isNaN(currentLine[j])) {
-					resString += currentLine[j];
-				} else {
-					resString = Wrapper(resString, currentLine[j]);
-				}
-			}
-			resString += 'TW';
-			operations[operations.length] = resString;
-			resString = '';
-
-			console.log(operations);
-			break;
-
-		case "FIRE":
-			console.log('--- fire ---');
-
-			resString += 'FR';
-
-			for (let j = 1; j < currentLine.length; j++) {
-				if (isNaN(currentLine[j])) {
-					resString += currentLine[j];
-				} else {
-					resString = Wrapper(resString, currentLine[j]);
-				}
-			}
-			resString += 'RF';
-			operations[operations.length] = resString;
-			resString = '';
-
-			console.log(operations);
-			break;
-		
-		case "MIX":
-			console.log('--- water ---');
-
-			resString += 'MX';
-
-			for (let j = 1; j < currentLine.length; j++) {
-				if (isNaN(currentLine[j])) {
-					resString += currentLine[j];
-				} else {
-					resString = Wrapper(resString, currentLine[j]);
-				}
-			}
-			resString += 'XM';
-			operations[operations.length] = resString;
-			resString = '';
-
-			console.log(operations);
-			break;
-	}
-	console.log(resString);
+    
 }
-
+Console.WriteLine(operations[operations.Count - 1]);
