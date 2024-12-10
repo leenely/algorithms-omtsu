0ca08cae2ca0fd64055b2187a65386f89771233c
Alexander <yourflexium@proton.me>
2024-12-10 21:17:55 +0600

    10.12.2024: fix idention

--- first-grade/10.12.2024/obj/Debug/net8.0/10.12.2024.AssemblyInfo.cs
+++ first-grade/10.12.2024/obj/Debug/net8.0/10.12.2024.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("10.12.2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c3148ca7e61a775d49944e2f85cdf4560b1add9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d94009ca3d59a384899bbb1237f06d5f97271b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("10.12.2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("10.12.2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
