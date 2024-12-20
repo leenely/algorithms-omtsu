dc4b8fd4185bc3003faddaa88f5ae9d55d77c363
Alexander <yourflexium@protonmail.com>
2024-12-20 19:29:18 +0600

    rgr: final

--- first-grade/rgr/Крестьянин и чёрт/obj/Debug/net8.0/villager.AssemblyInfo.cs
+++ first-grade/rgr/Крестьянин и чёрт/obj/Debug/net8.0/villager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("villager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0d5e4a7345ef7d7ae1350b51e3a7b74647a9f9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9875540c486c80d3db735b4db2cd5b712bdae074")]
 [assembly: System.Reflection.AssemblyProductAttribute("villager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("villager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
