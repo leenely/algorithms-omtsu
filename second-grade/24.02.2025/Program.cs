6a2a34ff7f7c48c0cc8eebaa089a443c78372337
leenely <yourflexium@protonmail.com>
2025-02-24 08:52:19 +0600

    24.02.2025: answer

---
+++ second-grade/24.02.2025/Program.cs
@@ -0,0 +1,39 @@
+﻿string line = Console.ReadLine();
+if (line == "")
+{
+	Console.WriteLine("Строка пуста");
+	return;
+}
+
+Stack<char> stack = new Stack<char>();
+foreach (char c in line)
+{
+	if (c == '(' || c == '[' || c == '{')
+	{
+		stack.Push(c);
+	}
+	else if (c == ')' || c == ']' || c == '}')
+	{
+		if (stack.Count == 0) 
+		{
+			Console.WriteLine("Стэк пуст");
+			return;
+		}
+
+		char nowSymbol = stack.Pop();
+		if ((nowSymbol == '(' && c != ')') || (nowSymbol == '[' && c != ']') || (nowSymbol == '{' && c != '}'))
+		{
+			Console.WriteLine("Скобки расставлены неправильно");
+			return;
+		}
+	}
+}
+
+if (stack.Count != 0) 
+{
+	Console.WriteLine("Скобки расставлены неправильно");
+}
+else
+{
+	Console.WriteLine("Скобки расставлены правильно");
+}
