9bb5fd19834336ff7c89433ed7131c8e7c3439e3
leenely <yourflexium@protonmail.com>
2025-03-22 11:53:59 +0600

    27.02.2025: answer

---
+++ second-grade/27.02.2025/Program.cs
@@ -0,0 +1,56 @@
+﻿Stack<Int32> stack = new Stack<Int32>();
+
+int MakeOperation(string operation, int op_1, int op_2)
+{
+  switch (operation)
+  {
+    case "+":
+      return op_1 + op_2;
+    case "-":
+      return op_1 - op_2;
+    case "*":
+      return op_1 * op_2;
+    case "/":
+      if (op_2 == 0)
+      {
+        throw new DivideByZeroException();
+      }
+      return op_1 / op_2;
+    default:
+      throw new ArgumentException();
+  }
+}
+
+
+Console.WriteLine("Введите выражение: ");
+string input = Console.ReadLine();
+string[] chars = input.Split(' ');
+
+foreach (string symbol in chars)
+{
+  if (symbol == "+" || symbol == "-" || symbol == "*" || symbol == "/")
+  {
+    if (stack.Count >= 2)
+    {
+      int op_2 = stack.Pop();
+      int op_1 = stack.Pop();
+
+      int result = MakeOperation(symbol, op_1, op_2);
+      stack.Push(result);
+    }
+  }
+  else
+  {
+    if (int.TryParse(symbol, out int isymbol))
+    {
+      stack.Push(isymbol);
+    }
+  }
+}
+
+if (stack.Count != 1)
+{
+  throw new ArgumentException();
+}
+
+Console.WriteLine($"Итого: {stack.Pop()}");
