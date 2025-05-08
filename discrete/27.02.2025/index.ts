10e1501b476bd2c9f9adbb7c3f278f513c3428d2
leenely <yourflexium@protonmail.com>
2025-05-08 19:49:08 +0600

    27.02.2025: answer update

--- discrete/27.02.2025/index.ts
+++ discrete/27.02.2025/index.ts
@@ -1,42 +1,40 @@
-export function Dijkstra(matrix: number[][], start: number, end: number) {
-	const n = matrix.length;
-	start = start - 1
-	end = end - 1
-	
-	const fetchedMatrix = matrix.map((row) => {
-		return row.map((el) => {
-			if (el === 0) return Infinity
-			return el
-		})
-	})
-	
-	const distances = new Array(n).fill(Infinity)
-	const visitedNodes = new Array(n).fill(false)
-	distances[start] = 0
+export function Dijkstra(matrix, start, end) {
+  const n = matrix.length
+  const distances = new Array(n + 1).fill(Infinity)
+  distances[start] = 0
 
-	for (let i = 0; i < n; i++) {
-		let minDistance = Infinity
-		let nodeNow = -1
+  const visitedNodes = new Array(n + 1).fill(false)
 
-		for (let j = 0; j < n; j++) {
-			if (!visitedNodes[j] && distances[j] < minDistance) {
-				minDistance = distances[j]
-				nodeNow = j
-			}
-		}
+  const fetchedMatrix = matrix.map(row =>
+    row.map(el => (el === 0 ? Infinity : el))
+  )
 
-		if (nodeNow === -1) break;
-		visitedNodes[nodeNow] = true
+  for (let i = 1; i <= n; i++) {
+    let nodeNow = -1
 
-		for (let k = 0; k < n; k++) {
-			if (fetchedMatrix[nodeNow][k] !== Infinity && !visitedNodes[k]) {
-				if (distances[nodeNow] + fetchedMatrix[nodeNow][k] < distances[k]) {
-					distances[k] = distances[nodeNow] + fetchedMatrix[nodeNow][k]
-				}
-			}
-		}
-	}
-	
-	if (distances[end] === Infinity) return -1;
-	return distances[end]
+    for (let j = 1; j <= n; j++) {
+      if (!visitedNodes[j]) {
+        if (nodeNow === -1 || distances[j] < distances[nodeNow]) {
+          nodeNow = j
+        }
+      }
+    }
+
+    if (distances[nodeNow] === Infinity) break
+
+    visitedNodes[nodeNow] = true
+
+    for (let k = 1; k <= n; k++) {
+      if (fetchedMatrix[nodeNow - 1][k - 1] !== Infinity && !visitedNodes[k]) {
+        if (
+          distances[nodeNow] + fetchedMatrix[nodeNow - 1][k - 1] <
+          distances[k]
+        ) {
+          distances[k] = distances[nodeNow] + fetchedMatrix[nodeNow - 1][k - 1]
+        }
+      }
+    }
+  }
+
+  return distances[end] === Infinity ? -1 : distances[end]
 }
