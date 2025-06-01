9dcb675ac5d43220ba5a102eb4a5f993078bf46e
leenely <yourflexium@protonmail.com>
2025-06-01 13:04:45 +0600

    22.05.2025: answers

---
+++ second-grade/22.05.2025/Program.cs
@@ -0,0 +1,30 @@
+﻿unsafe
+{
+    int* rate = stackalloc int[256];
+
+    Console.Write("Введите количество строк: ");
+    int n = int.Parse(Console.ReadLine());
+
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write("Введите строку: ");
+        string input = Console.ReadLine();
+        foreach (char c in input)
+        {
+            if (c < 256)
+            {
+                rate[c]++;
+            }
+        }
+    }
+
+    Console.WriteLine("Частоты: ");
+    for (int i = 0; i < 256; i++)
+    {
+        if (rate[i] > 0)
+        {
+            char ch = (char)i;
+            Console.WriteLine($"{ch}: {rate[i]}");
+        }
+    }
+}
