9bbc6a122a96069d4c91071173b61483e6d38d8e
leenely <yourflexium@protonmail.com>
2025-06-05 09:13:11 +0600

    02.06.2025: answer update

---
+++ second-grade/02.06.2025/Program.cs
@@ -0,0 +1,42 @@
+﻿// Объект: (структура) поле id_студента, поле ФИО, два указателя в структуре (один на левое поддерево, другой - на правое поддерево). На вход подаются объекты заданной структуры. Необходимо используя айди студента в качество ключевого поля построить бинарное дерево. Вывод как угодно, главное показать уровень дерева
+
+using System;
+
+struct Student
+{
+  public int id;
+  public string name;
+  public unsafe Student* left;
+  public unsafe Student* right;
+
+  public unsafe Student(int id, string name)
+  {
+    this.id = id;
+    this.name = name;
+    this.left = null;
+    this.right = null;
+  }
+
+  public string Log()
+  {
+    return $"[{id} {name}]";
+  }
+}
+
+class BinaryTree
+{
+  public unsafe Student* head;
+
+  public unsafe void Add(Student student)
+  {
+    Insert(ref head, student);
+  }
+
+  public unsafe void Insert(ref Student* node, Student student)
+  {
+    if (node == null)
+    {
+      node 
+    }
+  }
+}
